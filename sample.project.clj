a4cc68e1642802844aaf681f7d665a3694a3c9ff
snakevash <164011252@qq.com>
2015-03-11 17:59:21 +0800

    运行代码部分

--- sample.project.clj
+++ sample.project.clj
@@ -33,6 +33,16 @@
             ;; "do" 任务支持嵌套的 vectors
             ["do" "clean" ["test" ":integration"] ["deploy" "clojars"]]}
 
+  ;;; 运行项目代码
+  :prep-tasks [["protobuf" "compile"] "javac" "compile"]
+  :aot [org.example.sample]
+  :injections [(require 'clojure.pprint)]
+  :java-agents [[nodisassemble "0.1.1" :options "extra"]]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :warn-on-reflection true
+  :global-vars {*warn-on-reflection* true
+                *assert* false}
+
   ;;; 文件系统
   ;; 如果你用了一个不同的目录结构, 你可以设置这些.
   ;; 输入路径是包含字符串的 vector, 输出是字符串.
@@ -49,6 +59,26 @@
   ;; %s 表示当前配置名称
   ;; 注意 这不是查找本地库的地方, 使用 :jvm-opts 来替代 -Djava.library.path=...
   :native-path "%s/bits-n-stuff"
+  ;; `lein clean` 命令清除的文件夹.
+  ;; 用关键字和关键字链来指定.
+  ;; 单个路径和路径的集合都是被接受的.
+  ;; 例如
+  ;;   :target-path "target"
+  ;;   :compile-path "classes"
+  ;;   :foobar-paths ["foo" "bar"]
+  ;;   :baz-config {:qux-path "qux"}
+  ;; :clean-targets 会在 `lein clean` 移除文件
+  ;; "classes", "foo", "bar", "qux", and "out".
+  ;; 默认会保护 lein 的文件 ("src", "test", "resources", "doc", "project.clj")
+  ;; 但是, 这个保护会被 :clean-targets 的元数据覆盖 ^{:protect false}
   :clean-targets [:target-path :compile-path :foobar-paths
                   [:baz-config :qiu-path] "out"]
+  ;; 这是一个依赖的问题 (让我先看看)
+  :clean-non-project-classes true
+  ;; 在 checkout 中的路径会包含在每个项目的 classpath 中.
+  ;; (在FAQ有详细的信息.)
+  ;; 项目会把这个 vector 中的函数当做参数. 默认值为 [:source-paths :compile-path :resource-paths],
+  ;; 你可以使用下面的来共享代码到你的测试套件中:
+  :checkout-deps-shares [:source-paths :test-paths
+                         ~(fn [p] (str (:root p) "/lib/dev/*"))]
   )
